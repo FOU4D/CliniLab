fded5223b881ee55e2ac6507b6e2ca561bed8093
fou4d <fakhruldeen@gmail.com>
2020-08-29 02:02:55 +0000

    frontend visits

--- resources/views/visit/show.blade.php
+++ resources/views/visit/show.blade.php
@@ -15,7 +15,8 @@
 	<section class="container-fluid d-print-none">
     	<a href="javascript: window.print();" class="btn float-right"><i class="la la-print"></i></a>
 		<h2>
-	        <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
+			<i class="la la-th-list la-lg"></i>
+	        <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name !!}</span>
 	        <small>{!! $crud->getSubheading() ?? mb_ucfirst(trans('backpack::crud.preview')).' '.$crud->entity_name !!}.</small>
 	        @if ($crud->hasAccess('list'))
 	          <small class=""><a href="{{ url($crud->route) }}" class="font-sm"><i class="la la-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a></small>
@@ -28,7 +29,7 @@
 <div id="headerfull" class="row d-flex">
 	<div id="visitinfo" class="col-sm-12 col-md-6">
 		<div class="card">
-			<div class="card-header bg-primary">
+			<div class="card-header bg-orange text-white">
 				<i class="la la-barcode"></i>
 				<strong>The Visit:</strong> {{$entry->id}}
 			</div>
@@ -36,11 +37,11 @@
 				<ul class="list-group">
 					@isset($entry->referenced_by)
 					<li class="list-group-item">
-						<strong>Referenced By: </strong>{{$entry->referenced_by}}
+						<i class="la la-plus-square la-lg"></i><strong>Referenced By: </strong>{{$entry->referenced_by}}
 					</li>
 					@endisset
 					<li class="list-group-item">
-						<strong>Time and Date: </strong>{{$entry->created_at}}
+						<i class="la la-calendar la-lg"></i><strong>Time and Date: </strong>{{$entry->created_at}}
 					</li>
 				</ul>
 			</div>
@@ -48,14 +49,14 @@
 	</div>
 	<div id="visitorinfo" class="col-sm-12 col-md-6">
 		<div class="card">
-			<div class="card-header bg-success">
-				<i class="la la-users"></i>
+			<div class="card-header bg-blue text-white">
+				<i class="la la-barcode la-lg"></i>
 				<strong>The Visitor:</strong> {{$entry->individual_id}}
 			</div>
 			<div class="card-body">
 				<ul class="list-group">
 					<li class="list-group-item">
-						<strong>Name: </strong> <a href="/lab/individual/{{$entry->individual['id']}}/show">{{$entry->individual['name']}}</a>
+						<i class="la la-user la-lg"></i><strong>Name: </strong> <a href="/lab/individual/{{$entry->individual['id']}}/show">{{$entry->individual['name']}}</a>
 					</li>
 			      @php
                     $datezx = new DateTime($entry->individual['dob']);
@@ -70,7 +71,7 @@
                   @endphp
 
 					<li class="list-group-item">
-						<strong>Gender: </strong>{{$entry->individual['gender']}}, <strong>Age: </strong>{{$agefzx}} years.
+						<i class="la la-venus-mars la-lg"></i><strong>Gender: </strong>{{$entry->individual['gender']}}, <i class="la la-calendar la-lg"></i> <strong>Age: </strong>{{$agefzx}} years.
 					</li>
 				</ul>
 			</div>
@@ -81,8 +82,8 @@
 <div id="testsrequired" class="row d-flex">
 	<div class="col-sm-12 col-md-12">
 		<div class="card">
-			<div class="card-header bg-warning">
-				<i class="la la-users"></i>
+			<div class="card-header bg-gray-500">
+				<i class="la la-eyedropper la-lg"></i>
 				<strong>{{count($entry->request)}} Test Requested</strong>
 			</div>
 			<div class="card-body">
@@ -90,33 +91,31 @@
 			  <thead>
 			    <tr>
 			    	<th>Req. ID</th>
-			    	<th>Date & Time</th>
 			    	<th>Req Test</th>
-			    	<th>Sample Status</th>
+			    	<th>Date & Time</th>
 			    </tr>
 			  </thead>
 			  <tbody>
 			    @foreach ($entry->request as $requestzx)
 			    <tr>
-			      <td><a href="/lab/request/{{$requestzx->id}}/show">{{$requestzx->id}}</a></td>
-			      <td>{{$requestzx->created_at}}</td>
-			      <td><a href="/lab/test/{{$requestzx->test_id}}/show">{{$requestzx->test_id}}</a></td>
-			      <td>{{$requestzx->sample_status}}</td>
+			      <td><i class="la la-barcode la-lg"></i><a href="/lab/request/{{$requestzx->id}}/show">{{$requestzx->id}}</a></td>
+			      <td><i class="la la-flask la-lg"></i><a href="/lab/test/{{$requestzx->test_id}}/show">{{$requestzx->test_id}}</a></td>
+			      <td><i class="la la-calendar la-lg"></i>{{$requestzx->created_at}}</td>
 			    </tr>
 			    @endforeach
 			  </tbody>
 			</table>
-<a class="card-header-action btn-setting" href="/lab/request/create?visit_id={{$entry->id}}">
+				<a class="card-header-action btn-setting" href="/lab/request/create?visit_id={{$entry->id}}">
 				<i class="la la-plus-square la-lg"></i> Add Test Request</a>
 			</div>
 		</div>
 	</div>
 </div>
-<div id="footnotes" class="row d-flex">
+<div id="footnotesss" class="row d-flex">
 	<div class="col-sm-12 col-md-12">
 		<div class="card">
-			<div class="card-header bg-warning">
-				<i class="la la-users"></i>
+			<div class="card-header bg-yellow">
+				<i class="la la-exclamation-triangle la-lg"></i>
 				<strong>Notes</strong>
 			</div>
 			<div class="card-body">
